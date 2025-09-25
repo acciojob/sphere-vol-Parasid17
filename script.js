5d38d0d262c78e996f03fe22a56adc0f79fdc441
Siddhant Herale <154230247+Parasid17@users.noreply.github.com>
2025-09-25 11:07:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+	let radius = document.getElementById('radius').value;
+	let volume = document.getElementById('volume');
+
+	if (isNaN(radius) || Number(radius) < 0) {
+		volume.value = "NaN";
+	}
+	else {
+		let vol = ((4 / 3) * Math.PI * Math.pow(Number(radius), 3)).toFixed(4);
+		volume.value = vol;
+	}
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
